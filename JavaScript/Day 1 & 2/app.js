ce2cd17a4575bd62b00c84cba63df25637795a83
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-01-20 22:39:18 +0530

    Completed JavaScript Assingment 1

---
+++ JavaScript/Day 1 & 2/app.js
@@ -0,0 +1,85 @@
+// Question 1:
+
+// let num = 100;
+
+// if (num % 10 == 0) {
+//     console.log("good");
+// } else {
+//     console.log("bad");
+// }
+
+
+// Question 2:
+
+// let name = prompt("Enter your name:");
+// let age = prompt("Enter your age:");
+
+// alert(`${name} is ${age} years old.`);
+
+
+// Question 3:
+
+// let quarter = 3;
+// switch(quarter) {
+//     case 1:
+//         console.log("January , February , March");
+//         break;
+//     case 2:
+//         console.log("April , May , June");
+//         break;
+//     case 3:
+//         console.log("July , August , September");
+//         break;
+//     case 4:
+//         console.log("October , November , December");
+//         break;
+//     default :
+//         console.log("Wrong Input");
+// }
+
+
+// Question 4:
+
+// let string = "Apples";
+
+// if ((string[0]== 'A' || string[0] == 'a') && string.length > 5) {
+//     console.log("It is a Golden String");
+// } else {
+//     console.log("is NOT a golden string");
+// }
+
+
+// Question 5:
+
+// let num1 = 11;
+// let num2 = 28;
+// let num3 = 31;
+
+// if (num1 > num2) {
+//     if (num1 > num3) {
+//         console.log("largest is:" , num1);
+//     } else {
+//         console.log("largest is:" , num3);
+//     }
+// } else {
+//     if (num2 > num3) {
+//         console.log("largest is:" , num2);
+//     } else {
+//         console.log("largest is:" , num3);
+//     }
+// }
+
+
+// Question 6:
+
+// let num1 = 32;
+// let num2 = 47852;
+
+// let ld1 = num1 % 10;
+// let ld2 = num2 % 10;
+
+// if (ld1 == ld2) {
+//     console.log("SAME");
+// } else {
+//     console.log("NOT SAME");
+// }
