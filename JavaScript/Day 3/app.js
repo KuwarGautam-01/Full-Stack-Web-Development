481e9eed5d4062d25584312760a366949f0c05ba
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-02-06 19:51:38 +0530

    JavaScript Assignment 2 DONE

---
+++ JavaScript/Day 3/app.js
@@ -0,0 +1,45 @@
+// question 1
+
+// let array = [7,9,0,2];
+
+// console.log(array.slice(0,3));
+
+
+// question 2
+
+// console.log(array.slice(1));
+
+
+// question 3
+
+// let string = "gautam";
+
+// if (string.length == 0) {
+//     console.log("BLANK");
+// } else {
+//     console.log("NOT BLANK");
+// }
+
+
+// question 4
+
+// let string = "GauTam";
+
+// if(string[2] == string.toLowerCase()[2]) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+
+
+// question 5
+
+// let string = "   Jayesh.  ";
+
+// console.log(string.trim());
+
+// question 6
+
+let array = [1,2,3,4,6,7,8];
+
+console.log(array.includes(8));
