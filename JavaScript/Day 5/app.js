a2c10b22393b1a114359a2e1a470576103072861
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-02-08 00:45:06 +0530

    JavaScript Assignment 4 DONE

---
+++ JavaScript/Day 5/app.js
@@ -0,0 +1,30 @@
+// question 1
+
+// const random = Math.floor(Math.random() * 6) + 1;
+
+// console.log(random);
+
+
+// question 2
+
+// const car = {
+//     name : "Mahindra THAR 4X4" ,
+//     model : 2026 ,
+//     color : "white"
+// }
+
+// console.log(car.name);
+
+
+// question 3
+
+const person = {
+    name : "Jayesh" ,
+    age : 52 , 
+    city : "Nandurbar"
+}
+
+person.city = "Gurgaon";
+person.country = "India";
+
+console.log(person);
