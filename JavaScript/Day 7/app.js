3c89170a79225b9155fe10191c0e95c59df4bde9
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-02-08 15:54:10 +0530

    JavaScript Assignment 6 DONE

---
+++ JavaScript/Day 7/app.js
@@ -0,0 +1,41 @@
+// question 1
+
+// let arr = [1,2,3,4,5];
+// let sum = 0;
+
+// const arrayAverage = (arr) => {
+//     for(num of arr) {
+//         sum += num;
+//     }
+
+//     let avg = sum/arr.length;
+
+//     return avg;
+// }
+
+// console.log(arrayAverage(arr));
+
+
+//question 2
+
+// let num = 18;
+
+// const isEven = (num) => {
+//     if(num % 2 == 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+
+// console.log(isEven(num));
+
+
+// question 3
+
+// Hello, World!
+
+
+// question 4
+
+// undefined
